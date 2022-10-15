7d7d5c52bcfb9ff2c67786788d710a8f3f6a903c
Kevin Roy Lima <cknl360@gmail.com>
2022-10-15 17:03:02 +0200

    Initial Commit

---
+++ Bookshelf/BookshelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfApp.swift
+//  Bookshelf
+//
+//  Created by Kevin Lima on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
